c688bd867be31ce04ae40a0c8ad8aa901aeb3f7b
Lucas Fernandes <lucasfernandescs@outlook.com>
2026-04-13 14:44:00 -0300

    feat(reservation): implement core validation rules and overlap checks

---
+++ src/services/ReservationService.js
@@ -0,0 +1,52 @@
+const ReservationService = {
+  createReservation: (dados, reservasExistentes = []) => {
+    const diasProibidos = [0, 6];
+    console.log("Valor recebido:", reservasExistentes);
+    const temConflito = reservasExistentes.some((reservaAntiga) => {
+      return (
+        dados.dataInicio < reservaAntiga.dataFinal &&
+        dados.dataFinal > reservaAntiga.dataInicio
+      );
+    });
+
+    if (dados.dataInicio < new Date()) {
+      throw new Error("A data de início não pode estar no passado");
+    }
+
+    if (dados.dataFinal < dados.dataInicio) {
+      throw new Error(
+        "O horário final da reserva não pode vir antes do horário inicial",
+      );
+    }
+
+    if (
+      dados.dataInicio.getHours() < 8 ||
+      dados.dataFinal.getHours() > 18 ||
+      (dados.dataFinal.getHours() === 18 && dados.dataFinal.getMinutes() > 0)
+    ) {
+      throw new Error("A reserva só pode ser feita em horário comercial");
+    }
+
+    if (
+      diasProibidos.includes(dados.dataInicio.getDay()) ||
+      diasProibidos.includes(dados.dataFinal.getDay())
+    ) {
+      throw new Error("A reserva não pode ser feita nos finais de semana");
+    }
+
+    if ((dados.dataFinal - dados.dataInicio) / (1000 * 60 * 60) > 4) {
+      throw new Error("A reserva não pode durar mais do que 4 horas");
+    }
+
+    if (temConflito) {
+      throw new Error("A sala já está reservada neste horário");
+    }
+
+    return {
+      message: "Reserva realizada com sucesso!",
+      reserva: dados,
+    };
+  },
+};
+
+module.exports = ReservationService;

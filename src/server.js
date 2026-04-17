e63c725810a695b1fa34e641f895748836d29e23
Lucas Fernandes <lucasfernandescs@outlook.com>
2026-04-17 01:40:53 -0300

    fix(server): add dynamic port for deployment

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/", ReservationRoutes);
 
 if (require.main === module) {
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
